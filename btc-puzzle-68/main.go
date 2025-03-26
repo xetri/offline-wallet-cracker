3a00c70de8a52308834401e7cbe9baa8dcbf06f7
xetri <xetrymebich@gmail.com>
2025-03-26 14:16:05 +0545

    Compare pkhash instead of address

--- btc-puzzle-68/main.go
+++ btc-puzzle-68/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"crypto/rand"
 	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
 	"math/big"
 	"os"
@@ -22,49 +23,64 @@ const (
 
 var (
     numThreads = runtime.NumCPU()
-    DIFF = big.NewInt(128)
+    DIFF = big.NewInt(3966)
+    targetPKhash = addressToPublicKeyHash(TARGET_COMP_ADDR)
 )
 
-// Generate Bitcoin address from private key
-func generateAddress(privKey *big.Int) string {
-	// Convert private key to ECDSA format
-	ecdsaPrivKey, _ := btcec.PrivKeyFromBytes(privKey.Bytes())
+func main() {
+    fmt.Println("Searching:>")
 
-	// Get public key (compressed)
+    min := new(big.Int)
+    min.SetString(StartHex, 16)
+    max := new(big.Int)
+    max.SetString(EndHex, 16)
+    rangeDiff := new(big.Int).Add(new(big.Int).Sub(max, min), big.NewInt(1))
+
+    for {
+        var wg sync.WaitGroup
+        for range numThreads {
+            wg.Add(1)
+            go func() {
+                defer wg.Done()
+
+                random, _ := rand.Int(rand.Reader, rangeDiff)
+                random = new(big.Int).Add(min, random)
+                scan(random)
+            }()
+        }
+        wg.Wait()
+    }
+}
+
+func addressToPublicKeyHash(addr string) string {
+    hash := base58.Decode(addr)
+    pkhash := hash[1:len(hash)-4]
+    return hex.EncodeToString(pkhash)
+}
+
+func privateKeyToPublicKeyHash(privKey *big.Int) string {
+	ecdsaPrivKey, _ := btcec.PrivKeyFromBytes(privKey.Bytes())
 	pubKey := ecdsaPrivKey.PubKey().SerializeCompressed()
 
 	// SHA-256 hash
 	sha := sha256.Sum256(pubKey)
-
 	// RIPEMD-160 hash
 	ripemd := ripemd160.New()
 	ripemd.Write(sha[:])
 	pubKeyHash := ripemd.Sum(nil)
 
-	// Add Bitcoin version byte (0x00 for mainnet)
-	versionedHash := append([]byte{0x00}, pubKeyHash...)
-
-	// Double SHA-256 for checksum
-	checksum := sha256.Sum256(versionedHash)
-	checksum = sha256.Sum256(checksum[:])
-
-	// Append first 4 bytes of checksum
-	finalHash := append(versionedHash, checksum[:4]...)
-
-	// Convert to Base58 address
-	address := base58.Encode(finalHash)
-
-	return address
+    return hex.EncodeToString(pubKeyHash)
 }
 
-func scanDiff(basenum *big.Int) {
+func scan(basenum *big.Int) {
 	current := new(big.Int).Sub(basenum, DIFF)
     final := new(big.Int).Add(basenum, DIFF)
 	one := big.NewInt(1)
 
 	for current.Cmp(final) <= 0 {
-		address := generateAddress(current)
-		if address == TARGET_COMP_ADDR {
+		pkhash := privateKeyToPublicKeyHash(current)
+
+		if pkhash == targetPKhash {
             fmt.Printf("FOUND ^&^: %s\n", current.Text(16))
             saveToFile(current.Text(16))
             os.Exit(0)
@@ -101,28 +117,34 @@ func RandInt(min, max *big.Int) *big.Int {
     return new(big.Int).Add(random, min)
 }
 
-func scan() {
-	start := new(big.Int)
-	end := new(big.Int)
-
-	start.SetString(StartHex, 16)
-	end.SetString(EndHex, 16)
-
-	var wg sync.WaitGroup
-	for range numThreads {
-        wg.Add(1)
-		go func() {
-            defer wg.Done()
-            random := RandInt(start, end)
-            scanDiff(random)
-        }()
-	}
-    wg.Wait()
-}
-
-func main() {
-    fmt.Println("Searching:>")
-    for {
-        scan()
-    }
-}
+// Generate Bitcoin address from private key
+// func generateAddress(privKey *big.Int) string {
+// 	// Convert private key to ECDSA format
+// 	ecdsaPrivKey, _ := btcec.PrivKeyFromBytes(privKey.Bytes())
+// 
+// 	// Get public key (compressed)
+// 	pubKey := ecdsaPrivKey.PubKey().SerializeCompressed()
+// 
+// 	// SHA-256 hash
+// 	sha := sha256.Sum256(pubKey)
+// 
+// 	// RIPEMD-160 hash
+// 	ripemd := ripemd160.New()
+// 	ripemd.Write(sha[:])
+// 	pubKeyHash := ripemd.Sum(nil)
+// 
+// 	// Add Bitcoin version byte (0x00 for mainnet)
+// 	versionedHash := append([]byte{0x00}, pubKeyHash...)
+// 
+// 	// Double SHA-256 for checksum
+// 	checksum := sha256.Sum256(versionedHash)
+// 	checksum = sha256.Sum256(checksum[:])
+// 
+// 	// Append first 4 bytes of checksum
+// 	finalHash := append(versionedHash, checksum[:4]...)
+// 
+// 	// Convert to Base58 address
+// 	address := base58.Encode(finalHash)
+// 
+// 	return address
+// }
